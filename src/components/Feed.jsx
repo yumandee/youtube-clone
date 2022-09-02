1acc9234c82905e979aaada38faf151ae9ead890
Mandy Yu <yumandee@yahoo.com>
2022-09-02 15:53:57 -0400

    adding components for video and channel cards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,9 +7,11 @@ import { Sidebar, Videos } from './';
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([])
 
   useEffect(() => {
-    fetchFromAPI(`/search?part=snippet&q=${selectedCategory}`)
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
   }, [selectedCategory]);
 
   return (
@@ -57,7 +59,7 @@ const Feed = () => {
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
           {selectedCategory} <span style={{ color:'#f31503' }}> videos </span>
         </Typography>
-        <Videos />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
