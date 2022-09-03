ed9372ceddc780d2192c9149e0b9c2bd8ffff158
Mandy Yu <yumandee@yahoo.com>
2022-09-03 17:07:21 -0400

    implemented video detail page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,12 +6,12 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 
 const VideoCard = ({ video: { id: { videoId }, snippet} }) => {
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px',  }, boxShadow: 'none', borderRadius: 0 }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl }
           alt={snippet?.title}
-          sx={{ width: '358px', height: '180px' }}
+          sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: '180px' }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
